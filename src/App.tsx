7161ac49d6092eda692b0520c6db4685f5bd913b
Praneeth Edubilli <pedubilli@enphaseenergy.com>
2023-06-01 12:08:39 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const [val, setVal] = useState<string>("Hi, please delete this and enter a value")
+
+  const onChange = (x: { target: { value: string; }; }) => {
+    setVal(x.target.value)
+  }
+
+  return (
+    <div className="App">
+      <input onChange={onChange} value={val}/>
+      <p>{val}</p>
+    </div>
+  );
+}
+
+export default App;
